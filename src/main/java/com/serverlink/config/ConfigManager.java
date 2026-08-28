ff6ec6a90b3f31be2d0f5a204b41cc89456a7a58
Fmui <3948063970@qq.com>
2026-08-28 21:58:47 +0800

    Create ConfigManager.java

---
+++ src/main/java/com/serverlink/config/ConfigManager.java
@@ -0,0 +1,52 @@
+package com.serverlink.config;
+
+import com.serverlink.Main;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigManager {
+    private File commandsFile;
+    private FileConfiguration commandsConfig;
+
+    private File serverDataFile;
+    private FileConfiguration serverDataConfig;
+
+    public void loadAll() {
+        File dataFolder = Main.getInstance().getDataFolder();
+
+        commandsFile = new File(dataFolder, "commands.yml");
+        if (!commandsFile.exists()) {
+            Main.getInstance().saveResource("commands.yml", false);
+        }
+        commandsConfig = YamlConfiguration.loadConfiguration(commandsFile);
+
+        serverDataFile = new File(dataFolder, "servers-data.yml");
+        if (!serverDataFile.exists()) {
+            try {
+                serverDataFile.createNewFile();
+            } catch (IOException e) {
+                Main.getInstance().getLogger().severe("无法创建servers-data.yml！");
+            }
+        }
+        serverDataConfig = YamlConfiguration.loadConfiguration(serverDataFile);
+    }
+
+    public void saveServerData() {
+        try {
+            serverDataConfig.save(serverDataFile);
+        } catch (IOException e) {
+            Main.getInstance().getLogger().severe("保存servers-data.yml失败:" + e.getMessage());
+        }
+    }
+
+    public FileConfiguration getCommandsConfig() {
+        return commandsConfig;
+    }
+
+    public FileConfiguration getServerDataConfig() {
+        return serverDataConfig;
+    }
+}
